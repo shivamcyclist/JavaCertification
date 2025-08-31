8dfb81f7fe4ffdc912973080c9a28c1b7c1c64a6
shivam <shivam12397@gmail.com>
2025-08-31 23:46:40 +0530

    Refactoring Multiplication table class.

--- JavaIn28Mins/src/com/in28minutes/firstJavaProject/MultiplicationTable.java
+++ JavaIn28Mins/src/com/in28minutes/firstJavaProject/MultiplicationTable.java
@@ -3,16 +3,16 @@ package com.in28minutes.firstJavaProject;
 public class MultiplicationTable {
 
 	void print() {
-		for (int i = 1; i <= 10; i++) {
-			System.out.printf("%d * %d = %d", 5, i, 5 * i).println();// can be also written as System.out.printf("%d *
-																		// %d = %d",5,i,5 * i).println();
-		}
-		System.out.println();
+		print(5);
 	}
 
 	void print(int table) {
-		for (int i = 1; i <= 10; i++) {
-			System.out.printf("%d * %d = %d", table, i, table * i).println();
+		print(table, 1, 10);
+	}
+
+	void print(int table, int from, int to) {
+		for (int i = from; i <= to; i++) {
+			System.out.printf("%d X %d = %d", table, i, table * i).println();
 		}
 		System.out.println();
 	}
